8f2d532ad277aa1d72af214e4b6b76d46f270dcb
Rosa Tu <rosalina.tu@gmail.com>
2018-06-20 16:39:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,33 @@
 var  chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(a, b) {
- return a.unshift(b)
+function addElementToBeginningOfArray(a, b) 
+{
+ return [b, ...a]
+  }
+
+ function destructivelyAddElementToBeginningOfArray(a, b))
+ {
+   a.unshift(b)
+   return a
+ }
+ 
+ function accessElementInArray (a, b)
+ {
+   console.log(a[index])
+ }
+function destructivelyRemoveElementFromBeginningOfArray (a)
+{
+ array.shift()
+ return a
+}
+function removeElementFromBeginningOfArray(a)
+{
+  a.slice(1)
+  return a
+}
+
+function removeElementFromEndOfArray(a)
+  {
+    a.pop()
+    return a
   }
